b248394cbb7ce63668648127d3c43f86ef616af8
João Otávio <joaossousaa31@gmail.com>
2024-05-10 17:08:07 -0300

    feat: add the index.js

---
+++ 01-revisando-objetos/index.js
@@ -0,0 +1,20 @@
+//PascalCase
+function Book(title, pages, tags, author) {
+    this.title = title
+    this.pages = pages
+    this.tags = tags
+    this.author = author
+    this.inStock = false
+    this.addOnStock = function addOnStock(quantity) {
+        this.inStock += quantity
+    }
+    this.addPage = (quantity) => {
+        this.pages += quantity
+    }
+}
+
+const eragon = new Book('Harry Potter', 100, ['Fantasy', 'Action'], 'João Otávio')
+console.log(eragon)
+eragon.addOnStock(5)
+eragon.addPage(8)
+console.log(eragon)
