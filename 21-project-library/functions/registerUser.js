b8b7941265580d74d1b906ae9ebb280afcf1d423
João Otávio <joaossousaa31@gmail.com>
2024-06-03 17:06:56 -0300

    feat new project

--- 21-project-library/functions/registerUser.js
+++
@@ -1,8 +0,0 @@
-const User = require("../User")
-
-module.exports = function registerUser(username, adress, phone, library) {
-    const user = new User(username, adress, phone)
-    library.registerUser(user)
-
-    return user
-}
